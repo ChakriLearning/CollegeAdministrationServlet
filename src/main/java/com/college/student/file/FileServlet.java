a5d2a1f0e187f45a8bd4f52c349bfe3891ef0288
csb <>
2024-02-21 13:22:24 +0530

    Added Exception in FileServlet

--- src/main/java/com/college/student/file/FileServlet.java
+++ src/main/java/com/college/student/file/FileServlet.java
@@ -1,6 +1,7 @@
 package com.college.student.file;
 
-import com.college.student.controller.StudentServlet;
+import com.college.student.pojo.ErrorResponse;
+import com.google.gson.Gson;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.http.HttpServlet;
@@ -10,42 +11,58 @@ import jakarta.servlet.http.Part;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 
 @MultipartConfig(
-        fileSizeThreshold = 1024 * 1024 * 2,  // 2MB
-        maxFileSize = 1024 * 1024 * 10,       // 10MB
-        maxRequestSize = 1024 * 1024 * 50     // 50MB
 )
 public class FileServlet extends HttpServlet {
     private static final Logger logger = LoggerFactory.getLogger(FileServlet.class);
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String directoryPath = "C:\\Users\\chakr\\IdeaProjects\\CollegeAdministrationServlet\\";
-        int maxFileSize = 10 * 1024 * 1024; //10MB
-        String encoding = "UTF-8";
-        Part part = request.getPart("screenshot");
-        String fileName = part.getSubmittedFileName();
-        getServletContext().setAttribute("fileName", fileName);
-        String relativeFilePath = directoryPath + "\\" + fileName;
-        part.write(directoryPath + fileName);
-        PrintWriter out = response.getWriter();
-        response.sendRedirect("DownloadFile.html");
+        try {
+            Part part = request.getPart("screenshot");
+            String fileName = part.getSubmittedFileName();
+            getServletContext().setAttribute("fileName", fileName);
+            logger.info("Submitted File Name : {}", fileName + " Added to Context Level Attribute");
+            part.write(directoryPath + fileName);
+            logger.info("Saved File name to Disck");
+            response.sendRedirect("DownloadFile.html");
+        } catch (ServletException | IOException e) {
+            logger.info("Exception Occurred While Uploading File: {}", e.getMessage());
+            ErrorResponse errorResponse = new ErrorResponse(401, e.getMessage());
+            response.setContentType("application/json");
+            response.getWriter().println(new Gson().toJson(errorResponse));
+            response.getWriter().flush();
+        }
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String fileName = getServletContext().getInitParameter("fileName");
-
-        File file = new File("C:\\Users\\chakr\\IdeaProjects\\CollegeAdministrationServlet\\");
-        response.setContentType("application/octet-stream");
-        OutputStream out = response.getOutputStream();
-        FileInputStream in = new FileInputStream(file);
-        byte[] buffer = new byte[4096];
-        int length;
-        while ((length = in.read(buffer)) > 0) {
-            out.write(buffer, 0, length);
+        try {
+            String fileName = (String) getServletContext().getAttribute("fileName");
+            logger.info("File Name Retrieved From Context : {}", fileName);
+            File file = new File("C:\\Users\\chakr\\IdeaProjects\\CollegeAdministrationServlet\\" + fileName);
+            response.setContentType("application/octet-stream");
+            OutputStream out = response.getOutputStream();
+            FileInputStream in = new FileInputStream(file);
+            byte[] buffer = new byte[1000000000];
+            int length;
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+            logger.info("File Written to Output Stream for Download Successfully");
+            in.close();
+            out.flush();
+        } catch (IOException e) {
+            logger.info("Exception Occurred While Downloading File: {}", e.getMessage());
+            ErrorResponse errorResponse = new ErrorResponse(401, e.getMessage());
+            response.setContentType("application/json");
+            response.getWriter().println(new Gson().toJson(errorResponse));
+            response.getWriter().flush();
         }
-        in.close();
-        out.flush();
     }
 }
